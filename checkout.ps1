3205116061cd3a42a928d4ed5dc53bcfe8d1009b
Dmitry Krayniy <krdima92@gmail.com>
2022-05-18 14:45:05 +0300

    Added checkout script

---
+++ checkout.ps1
@@ -0,0 +1,14 @@
+#!/usr/bin/env pwsh
+
+git clone https://github.com/pip-services3-java/pip-services3-commons-java.git
+git clone https://github.com/pip-services3-java/pip-services3-components-java.git
+git clone https://github.com/pip-services3-java/pip-services3-container-java.git
+git clone https://github.com/pip-services3-java/pip-services3-data-java.git
+git clone https://github.com/pip-services3-java/pip-services3-rpc-java.git
+git clone https://github.com/pip-services3-java/pip-services3-messaging-java.git
+git clone https://github.com/pip-services3-java/pip-services3-elasticsearch-java.git
+git clone https://github.com/pip-services3-java/pip-services3-fluentd-java.git
+git clone https://github.com/pip-services3-java/pip-services3-memcached-java.git
+git clone https://github.com/pip-services3-java/pip-services3-redis-java.git
+git clone https://github.com/pip-services3-java/pip-services3-mongodb-java.git
+git clone https://github.com/pip-services3-java/pip-services3-prometheus-java.git

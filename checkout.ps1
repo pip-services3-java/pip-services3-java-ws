76974c86701f3e929dd15a382716dc0f39e6a4c4
Dmitry Krayniy <krdima92@gmail.com>
2022-08-12 16:26:12 +0400

    Added new toolkits. Use ssh

--- checkout.ps1
+++ checkout.ps1
@@ -1,15 +1,18 @@
 #!/usr/bin/env pwsh
 
-git clone https://github.com/pip-services3-java/pip-services3-commons-java.git
-git clone https://github.com/pip-services3-java/pip-services3-components-java.git
-git clone https://github.com/pip-services3-java/pip-services3-container-java.git
-git clone https://github.com/pip-services3-java/pip-services3-data-java.git
-git clone https://github.com/pip-services3-java/pip-services3-rpc-java.git
-git clone https://github.com/pip-services3-java/pip-services3-messaging-java.git
-git clone https://github.com/pip-services3-java/pip-services3-elasticsearch-java.git
-git clone https://github.com/pip-services3-java/pip-services3-fluentd-java.git
-git clone https://github.com/pip-services3-java/pip-services3-memcached-java.git
-git clone https://github.com/pip-services3-java/pip-services3-redis-java.git
-git clone https://github.com/pip-services3-java/pip-services3-mongodb-java.git
-git clone https://github.com/pip-services3-java/pip-services3-prometheus-java.git
-git clone https://github.com/pip-services3-java/pip-services3-expressions-java.git
+git clone git@github.com:pip-services3-java/pip-services3-commons-java.git
+git clone git@github.com:pip-services3-java/pip-services3-components-java.git
+git clone git@github.com:pip-services3-java/pip-services3-container-java.git
+git clone git@github.com:pip-services3-java/pip-services3-data-java.git
+git clone git@github.com:pip-services3-java/pip-services3-rpc-java.git
+git clone git@github.com:pip-services3-java/pip-services3-messaging-java.git
+git clone git@github.com:pip-services3-java/pip-services3-elasticsearch-java.git
+git clone git@github.com:pip-services3-java/pip-services3-fluentd-java.git
+git clone git@github.com:pip-services3-java/pip-services3-memcached-java.git
+git clone git@github.com:pip-services3-java/pip-services3-redis-java.git
+git clone git@github.com:pip-services3-java/pip-services3-mongodb-java.git
+git clone git@github.com:pip-services3-java/pip-services3-prometheus-java.git
+git clone git@github.com:pip-services3-java/pip-services3-expressions-java.git
+git clone git@github.com:pip-services3-java/pip-services3-mysql-java.git
+git clone git@github.com:pip-services3-java/pip-services3-swagger-java.git
+git clone git@github.com:pip-services3-java/pip-services3-grpc-java.git

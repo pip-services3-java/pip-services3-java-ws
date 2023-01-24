19c88447f1ef8b85d37e1555db71fa61f0755f07
Dmitriy Krayniy <dkrayniy@entinco.com>
2023-01-24 11:39:03 +0400

    Added push script

---
+++ push_changes.ps1
@@ -0,0 +1,24 @@
+#!/usr/bin/env pwsh
+
+param
+(
+    [Parameter(Mandatory=$true, Position=0)]
+    [string] $Commit
+)
+
+$repos = $(Get-ChildItem $PSScriptRoot -directory).Name
+foreach ($repo in $repos) {
+    Write-Host "Processing '$PSScriptRoot/$repo'"
+    Set-Location "$PSScriptRoot/$repo"
+
+    $gitStatus = git status
+    if ($gitStatus -Contains "nothing to commit, working tree clean") {
+        Write-Host "    Nothing to commit."
+        continue
+    }
+
+    Write-Host "    Pushing changes..."
+    git add .
+    git commit -m $Commit
+    git push
+}
